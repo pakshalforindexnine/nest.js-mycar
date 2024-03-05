f272f091fd427fb7adea1a9c6ac66101c230b1e9
pakshalforindexnine <pakshal.jain@indexnine.com>
2024-03-05 19:38:08 +0530

    5 March

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import {
+    CanActivate,
+    ExecutionContext
+} from '@nestjs/common'
+
+export class AuthGuard implements CanActivate{
+    canActivate(context: ExecutionContext) {
+        const request = context.switchToHttp().getRequest();
+
+        return request.session.userId;
+        
+        
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,54 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException} from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, ClassSerializerInterceptor, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
-  constructor(private userService: UsersService) {}
-  @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.userService.create(body.email, body.password);
+  constructor(
+    private userService: UsersService,
+    private authService: AuthService
+  ) {}
+
+  // @Get('/whoami')
+  // whoAmI(@Session() session: any){
+  //   return this.userService.findOne(session.userId)
+  // }
+
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto, @Session() session:any) {
+  const user = await this.authService.signup(body.email, body.password);
+  session.userId = user.id
+  return user;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+   const user = await this.authService.signin( body.email, body.password)
+   session.userId = user.id
+   return user;
+
+  }
+
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
 
   @Get('/:id')
   async findUser(@Param('id') id: string) {
